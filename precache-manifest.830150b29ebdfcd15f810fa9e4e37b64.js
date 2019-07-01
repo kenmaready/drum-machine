052c1630d97787e8cf55b9203c4ca79f66439a0c
kenmaready <ken@maready.com>
2019-07-01 15:23:48 -0400

    Updates

---
+++ precache-manifest.830150b29ebdfcd15f810fa9e4e37b64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78086d5d867a7d5e27bf62b8d8c8e611",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "2d436faae498d296bd19",
+    "url": "/drum-machine/static/css/main.a551c704.chunk.css"
+  },
+  {
+    "revision": "cb8cd8ba45c2aec18bbd",
+    "url": "/drum-machine/static/js/2.55fa59c1.chunk.js"
+  },
+  {
+    "revision": "2d436faae498d296bd19",
+    "url": "/drum-machine/static/js/main.2c831869.chunk.js"
+  },
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "fdff5d744cd21481c9ce57025116d26b",
+    "url": "/drum-machine/static/media/choir.fdff5d74.wav"
+  },
+  {
+    "revision": "0051aee512f51d3fac14cd6cb2feb643",
+    "url": "/drum-machine/static/media/crash_cymbal.0051aee5.wav"
+  },
+  {
+    "revision": "c673a53e83d22c9b14cfbe857c34beb5",
+    "url": "/drum-machine/static/media/drum_bass.c673a53e.wav"
+  },
+  {
+    "revision": "71b9af257cae3d27ea4652ed669d468c",
+    "url": "/drum-machine/static/media/drum_snare.71b9af25.wav"
+  },
+  {
+    "revision": "f6bed7fdb9e961ba93a48b686956538e",
+    "url": "/drum-machine/static/media/high_hat_closed.f6bed7fd.wav"
+  },
+  {
+    "revision": "e1792d035fb96adda9c0edde84f8b8bf",
+    "url": "/drum-machine/static/media/orchestra_hit.e1792d03.wav"
+  },
+  {
+    "revision": "fe1d4b95411714ddf9a522b00d92ddd9",
+    "url": "/drum-machine/static/media/orchestra_hit2.fe1d4b95.wav"
+  },
+  {
+    "revision": "c1c6fa3ff65708299990a9cdfa42bd29",
+    "url": "/drum-machine/static/media/tomtom_hi.c1c6fa3f.wav"
+  },
+  {
+    "revision": "5ae2532284a887b8706bce054cf531b2",
+    "url": "/drum-machine/static/media/tomtom_low.5ae25322.wav"
+  }
+]);
